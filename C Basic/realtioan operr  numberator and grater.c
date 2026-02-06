1a012521d59b51440cfa3ecdea7cbea967a6eed3
SOUNDHARYA <soundharya3veni@gmail.com>
2026-02-06 14:52:04 +0530

    Create realtioan operr  numberator and grater.c

---
+++ C Basic/realtioan operr  numberator and grater.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int a,b;
+    scanf("%d %d",a);
+    if(a>b)
+     printf("%d",a);
+    else if(b>a)
+     printf("%d",b);
+     else
+     printf("equal")
+    return 0;
+}
