fcf2b51cb5a26a8dbbeb00dc9b3c4333427476af
SOUNDHARYA <soundharya3veni@gmail.com>
2026-02-06 14:09:21 +0530

    Create sum of two numbers.c

---
+++ C Basic/sum of two numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int A, B;
+    
+    // Read two integers
+    scanf("%d %d", &A, &B);
+    
+    // Print their sum
+    printf("%d", A + B);
+    
+    return 0;
+}
