26ac0639b52099d71d8727a623fe0af1a74a5ba6
SOUNDHARYA <soundharya3veni@gmail.com>
2026-02-09 19:38:18 +0530

    Create temperature.c

---
+++ temperature.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int temp;
+    scanf("%d",&temp);
+    if( temp<=20);
+      printf("heating mode")
+    elseif temp<=30:
+      printf("nprmal mode")
+    else:
+     printf("cooling mode")
+    
