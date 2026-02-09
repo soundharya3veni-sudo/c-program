7f8a747bac6de654c2e4e41749abeb2a13da1d05
SOUNDHARYA <soundharya3veni@gmail.com>
2026-02-09 15:13:01 +0530

    Create data limit =.c

---
+++ C Basic/data limit =.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+  float data;
+  scanf("%f",&data);
+     if(data>2.0){
+          printf("data limit exceeded")
+     else{
+          printf("data within limit);
+     }
+     return 0;
+     }
+     
