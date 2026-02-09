32cbaa35c6f6d8af3cf2fd51a9cbb66a6ecfcdf7
SOUNDHARYA <soundharya3veni@gmail.com>
2026-02-09 19:18:56 +0530

    Create bank minimum balance.c

---
+++ bank minimum balance.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int main() {
+   int balance;
+   scanf("%d",&balance);
+   if(balace  < 1000){
+        printf("Low balance");
+   }else {
+        printf("sufficient balance");
+   }
+   return 0;
+}
