87e18f175094a515482ac866c4ff4ce4c2671fef
SOUNDHARYA <soundharya3veni@gmail.com>
2026-02-09 12:04:25 +0530

    Create salary.c

---
+++ C Basic/salary.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int salary;
+    scanf("%d",&salary);
+    if(salary>30000);{
+    printf("bonus eligible");
+   }
+   else{
+  printf("not eligible");
+  }
+  return 0;
+  
