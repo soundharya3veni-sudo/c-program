3e031d75850ffbf9e6e6d451b7dc2d97fa0a2afc
SOUNDHARYA <soundharya3veni@gmail.com>
2026-02-09 19:42:34 +0530

    Create water level.c

---
+++ C Basic/water level.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    int water level;
+    scanf("%d",&water level);
+    if( battery<=20);
+      printf("overflow warning")
+    elseif temp<=30:
+      printf("safe level")
