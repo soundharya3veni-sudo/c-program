aa42187fa65b0050b2a1a586824153903c5056ff
SOUNDHARYA <soundharya3veni@gmail.com>
2026-02-09 16:18:57 +0530

    Create vehicle problem.c

---
+++ C Basic/vehicle problem.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int main()  {
+    int day;
+    scanf("%d",&day);
+    if(day==6||day==7)
+    printf("weekend");
+    else
+    printf("weekday");
+    return 0;
+    
+}
