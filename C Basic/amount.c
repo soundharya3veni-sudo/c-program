ba7244670265a0c080e0edc3b4f6f59207db5db9
SOUNDHARYA <soundharya3veni@gmail.com>
2026-02-09 16:24:18 +0530

    Create amount.c

---
+++ C Basic/amount.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int main()  {
+    int account;
+    scanf("%d",&account);
+    if(account>8)
+    printf("account locked");
+    else
+    printf("login allowed");
+    return 0;
+    
+}
