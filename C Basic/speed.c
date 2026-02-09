af40f61f80470936eecdf00019a4583ff26551ca
SOUNDHARYA <soundharya3veni@gmail.com>
2026-02-09 16:22:20 +0530

    Create speed.c

---
+++ C Basic/speed.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int main()  {
+    int speed;
+    scanf("%d",&speed);
+    if(speed>8)
+    printf("speed limit exeded");
+    else
+    printf("with speed limit");
+    return 0;
+    
+}
