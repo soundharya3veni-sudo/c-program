1fc9d08b543f78c8314d5249a11b09562c7e1312
SOUNDHARYA <soundharya3veni@gmail.com>
2026-02-09 12:24:15 +0530

    Create check length.c

---
+++ C Basic/check length.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int length;
+    scanf("%d",&length);
+    if(length<=8);{
+    printf("strong numbers");
+   }
+   else{
+  printf("weak number");
+  }
+  return 0;
+}
+  
