6bba2efabffba8c95ffa6a6b0d46045f4e0bcdd2
SOUNDHARYA <soundharya3veni@gmail.com>
2026-02-14 13:51:36 +0530

    Create reverse of the star pattern.c

---
+++ C Basic/reverse of the star pattern.c
@@ -0,0 +1,11 @@
+    for (int i=1;i<=5;i++){
+     for(int s=1;s<=i+1;s++){
+       printf(" ");
+   }  
+     for(int j=1;j<=11-2*i;j++){
+       printf("*");
+   } 
+   printf("\n"); 
+  }
+    return 0;       
+}
