72b29242ed07aa273320f1ef7397c0db6190f30b
SOUNDHARYA <soundharya3veni@gmail.com>
2026-02-14 14:12:08 +0530

    Create pyramind on two loops.c

---
+++ C Basic/pyramind on two loops.c
@@ -0,0 +1,21 @@
+    for (int i=1;i<=5;i++){
+      for(int s=1;s<=5-i;s++){
+        printf(" ");
+    }  
+      for(int j=1;j<=2*i-1;j++){
+        printf("*");
+    } 
+    printf("\n"); 
+    }    
+    for (int i=1;i<=5;i++){
+       for(int s=1;s<=i+1;s++){
+         printf(" ");
+    }  
+        for(int j=1;j<=11-2*i;j++){
+          printf("*");
+    } 
+   printf("\n"); 
+    }
+    return 0;       
+}
+
