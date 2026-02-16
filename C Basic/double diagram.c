d2d6ba0265f22b92c3de33fcf5f16480539e7b31
SOUNDHARYA <soundharya3veni@gmail.com>
2026-02-16 15:48:01 +0530

    Create double diagram.c

---
+++ C Basic/double diagram.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+  int n;
+  scanf("%d",&n);
+    for (int i=1;i<=n;i++){
+     for(int j=1;j<=i;j++){
+       printf("%d",j);
+   }  
+    
+   for (int k=1;k<(n-i)*2;k++){
+       printf(" ");
+   }  
+    for(int j=1;j>=n;j--){
+       printf(" ");
+   } 
+   printf("\n"); 
+  }  
+}
