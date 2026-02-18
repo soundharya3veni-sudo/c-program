1e4960412ef2fe78764369dea2af02f7d3b9c7b3
SOUNDHARYA <soundharya3veni@gmail.com>
2026-02-18 16:29:27 +0530

    Create target elentss.c

---
+++ C Basic/target elentss.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+  for (int i=0;i<n;i++){
+    break;
+  }
+   count++;
+}
+if(idx!=-1){
+    printf("the element %d is int the array at index %d, target,idx);
+} 
+else{
+    printf("the element %d is not  in the array at index %d, target,idx)");
+} 
+     
+    return 0;
+}
