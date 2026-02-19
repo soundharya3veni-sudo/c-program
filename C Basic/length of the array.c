622346e47eca8726a38deaa6fca26d625444be1a
SOUNDHARYA <soundharya3veni@gmail.com>
2026-02-19 22:19:22 +0530

    Create length of the array.c

---
+++ C Basic/length of the array.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int count=0;
+    for(intj=0;j<size;j++){
+        if(arr[j]==1){
+            count ++;
+        }
+    }
+    printf("%d",were present",count);
+    return 0;
+}
