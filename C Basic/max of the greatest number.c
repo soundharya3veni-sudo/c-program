1ba2bb87f6d4f2e55d72fe2cecf6158f80fcdc6b
SOUNDHARYA <soundharya3veni@gmail.com>
2026-02-20 21:56:40 +0530

    Create max of the greatest number.c

---
+++ C Basic/max of the greatest number.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    int max = arr[n-1];
+    arr[n-1] = -1;
+
+    for(int i = n-2; i >= 0; i--) {
+        int temp = arr[i];
+        arr[i] = max;
+
+        if(temp > max)
+            max = temp;
+    }
+
+    for(int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
