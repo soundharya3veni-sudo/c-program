cac463489f646d8cf5f078406670f842de938fa4
SOUNDHARYA <soundharya3veni@gmail.com>
2026-02-21 21:19:19 +0530

    Create equlibrium index.c

---
+++ C Basic/equlibrium index.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+   int equlibrium index (int arr[], int n);
+   int total;
+   int left;
+   for(int i=0;i<n;i++){
+    total+= arr[i];
+   for(int i=0;i<n;i++){
+    total-= arr[i];
+   if(left== total){
+     return i;
+   }
+   left += arr[i];
+   }
+   return -1;
+   }
+   int main()
+   int n;
+   printf("enter number of elements:");
+   scanf("%d",n);
+    
+    
+    return 0;
+}
