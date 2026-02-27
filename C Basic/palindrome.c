3d1160a7123ce389a4e3097542975cacb0b94f0a
SOUNDHARYA <soundharya3veni@gmail.com>
2026-02-27 15:46:02 +0530

    Create palindrome.c

---
+++ C Basic/palindrome.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+    int n,temp,rem=0,rev=0;
+   printf("enter a number:");
+   scanf("%d",&n);
+    n=temp;
+    while(temp!=0){
+        rem=temp%10;
+        rev=rev*10+rem;
+        temp=temp/10;
+     {
+     }
+     printf("%d",rev);
+     if(n==rev);
+     printf("palindrome");
+     else
+     printf("not palindrome");
+    return 0;
+}
+    
+}
+  
