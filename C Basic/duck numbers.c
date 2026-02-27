c6d52288da940b2dc35d33f3c257991b7b050ef1
SOUNDHARYA <soundharya3veni@gmail.com>
2026-02-27 13:36:29 +0530

    Create duck numbers.c

---
+++ C Basic/duck numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() {
+  int n,temp,rem=0;
+  printf("enter the number");
+  scanf("%d",&n);
+  if(n==0);
+  printf("not duck number");
+  return 0;
+
+}
+  temp=n;
+  while(temp!=0);
+  rem=temp%10;
+  {
+  if(rem==0);
+  printf("duck number");
+    return 0;
+
+}
+ temp=temp/10; 
+ {
+ printf("not duck number");
+}
