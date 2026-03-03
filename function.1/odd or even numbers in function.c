10bb3c3079497ff0c4e651b25f936429926883e2
SOUNDHARYA <soundharya3veni@gmail.com>
2026-03-03 10:03:48 +0530

    Create odd or even numbers in function.c

---
+++ function.1/odd or even numbers in function.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main() {
+    int n;
+    printf("enter the  integer number\n");
+
+    scanf("%d",&n);
+    if(n%2==0)
+    printf("%d is a even numbers",n);
+    else
+    printf("%d is a odd nnumbers",n);
+    return ;
+}
