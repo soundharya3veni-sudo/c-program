e4e88089a48a4e9280dc63b01a9227163ee7a377
SOUNDHARYA <soundharya3veni@gmail.com>
2026-03-04 22:19:00 +0530

    Create palindrome .c

---
+++ function.1/palindrome .c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+   int n;
+   scanf("%d",&n);
+   while (n!=0)
+   if(n%2==0)
+   //printf("enter the number:");
+   printf("palindrome");
+   else
+   printf("not palindrome");
+    
+    return 0;
+}
