aa4b4e0f1ab306da77edc98b3727d623b2842e8d
SOUNDHARYA <soundharya3veni@gmail.com>
2026-03-05 14:43:27 +0530

    Create power calculation.c

---
+++ function.1/power calculation.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+double power(double a,int b){
+    if(b<0) return1.0;
+    if(b<0)return1.0{ power(a,-b) 
+    double res=1.0;
+    for(int i=0;i<=b;i++){
+        res*=a;
+    }
+    return res;
+    }
+    int main()
+    double(a,b)
+   scanf("%lf %lf",&a,&b);
+   printf("%llf",power a,b);
+    return 0;
+}
