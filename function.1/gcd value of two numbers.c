08ff2243b14bc570c01b93df28c537d0b251a74b
SOUNDHARYA <soundharya3veni@gmail.com>
2026-03-05 14:17:46 +0530

    Create gcd value of two numbers.c

---
+++ function.1/gcd value of two numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void GCDvalue(int a,int b)
+ while(num!=0){
+    int temp=num2;
+    num2=num1%num2;
+    num1=temp;
+ }
+ int n;
+ scanf("%d",&n);
+ printf(temp)
+ {
+
+    return 0;
+}
