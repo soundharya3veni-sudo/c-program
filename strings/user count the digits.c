418f1ed60a892675b04659a48aab589b7bc1969d
SOUNDHARYA <soundharya3veni@gmail.com>
2026-03-06 12:07:38 +0530

    Create user count the digits.c

---
+++ strings/user count the digits.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int ch;
+    int nc=0;
+    while((ch= get char())!eof)
+    nc=nc+1;
+    printf("number of character read is%d\n",nc);
+    getch();
+    return 0;
+}
