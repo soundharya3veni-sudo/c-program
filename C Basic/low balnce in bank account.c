d44cfa828de8d3f60edbd19b039948cf692b13e7
SOUNDHARYA <soundharya3veni@gmail.com>
2026-03-14 09:04:49 +0530

    Create low balnce in bank account.c

---
+++ C Basic/low balnce in bank account.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+   int N;
+   int balance;
+   int transcation;
+   int i=0;
+   int low balance;
+   scanf("%d",&n);
+   scanf("%d",&balance);
+   while(i<n){
+    scanf("%d",&transcation);
+    balance=balance+transcation;
+    if(balance<2000){
+        low days++;
+    }
+    i++;
+   }
+   printf("final balance:%d\n",balance);
+   printf("low balance day:%d\n",lowdays);
+   return 0;
+}
