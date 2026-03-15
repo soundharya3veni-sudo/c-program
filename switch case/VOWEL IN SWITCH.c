df231945bca55d36b9d30922b5b9a51bd9e8d191
SOUNDHARYA <soundharya3veni@gmail.com>
2026-03-15 19:17:27 +0530

    Create VOWEL IN SWITCH.c

---
+++ switch case/VOWEL IN SWITCH.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+  char ch;
+  printf("enter the char:");
+  scanf("%c",&ch);
+  switch(ch){
+    case'a':
+    case'e':
+    case'i':
+    case'o':
+    case'u':
+  }
+
+    printf("IT IS VOWEL");
+    break;
+    printf("ITS NOT A VOWEL");
+    
+    return 0;
+}
