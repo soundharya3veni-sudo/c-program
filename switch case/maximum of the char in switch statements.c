2b04f385c3f8fdfb4b5215f929d6b173860f67d9
SOUNDHARYA <soundharya3veni@gmail.com>
2026-03-15 19:21:17 +0530

    Create maximum of the char in switch statements.c

---
+++ switch case/maximum of the char in switch statements.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+  char ch;
+  printf("enter the char:");
+  scanf("%c",&ch);
+  switch(maximum of no>minmum of no){
+    case'a':
+    case'e':
+    case'i':
+    case'o':
+    case'u':
+  }
+
+    printf("IT IS maximum");
+    break;
+    printf("ITS NOT A maximum");
+    
+    return 0;
+}
