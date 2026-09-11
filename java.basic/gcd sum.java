2e5b14ea33c9e1a31d8edb0bae5c2613f4131e53
SOUNDHARYA <soundharya3veni@gmail.com>
2026-09-11 20:56:53 +0530

    Add GCD and divisor counting functionality
    Implement a method to count divisors of GCD of differences in an array.

---
+++ java.basic/gcd sum.java
@@ -0,0 +1,41 @@
+class Solution {
+
+    public int sameMod(int[] arr) {
+        int gcd = 0;
+
+        // Find GCD of differences
+        for (int i = 1; i < arr.length; i++) {
+            gcd = findGCD(gcd, Math.abs(arr[i] - arr[0]));
+        }
+
+        // All elements are equal
+        if (gcd == 0) {
+            return -1;
+        }
+
+        // Count divisors of gcd
+        int count = 0;
+
+        for (int i = 1; i * i <= gcd; i++) {
+            if (gcd % i == 0) {
+                count++;
+
+                if (i != gcd / i) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    private int findGCD(int a, int b) {
+        while (b != 0) {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+
+        return a;
+    }
+}
